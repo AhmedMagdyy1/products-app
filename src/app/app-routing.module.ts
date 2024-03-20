94c256880d53222416e956fef27cdcc0a549e34b
Ahmed Magdy <amagdy@enterpriseadvisory.com>
2024-03-20 23:21:22 +0200

    make the structure , handle routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products',component:ProductsComponent,title:'Products'},
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { ProductsComponent } from './pages/products/products.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
