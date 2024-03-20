7429a8534e910df0663c40122616d83d7a158231
Ahmed Magdy <amagdy@enterpriseadvisory.com>
2024-03-21 00:07:27 +0200

    make each card get a random color

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -9,6 +9,7 @@ import { ProductsService } from 'src/app/core/services/products.service';
 })
 export class ProductsComponent implements OnInit {
   allProducts:Products[]= []
+  cardColor = ['#FF8B64', '#55C2E6', '#FF5E7D', '#F1C75B', '#4BCF82']
   constructor(private _productsService:ProductsService){}
   ngOnInit(): void {
     this.loadProducts()
@@ -25,5 +26,10 @@ export class ProductsComponent implements OnInit {
         
       }
     })
+  }
+
+  getRandomColor(): string {
+    const randomIndex = Math.floor(Math.random() * this.cardColor.length);
+    return this.cardColor[randomIndex];
   }
 }
